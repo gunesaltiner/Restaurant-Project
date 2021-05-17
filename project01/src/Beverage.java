0e746be9f890c0ef34161c2a5713a3f1ff09754f
Güneş Altıner <gunes.altiner@ozu.edu.tr>
2021-05-17 20:06:44 +0300

    21-Beverage class

---
+++ project01/src/Beverage.java
@@ -0,0 +1,11 @@
+public class Beverage extends Product{
+
+    public Beverage(String name, double sellingPrice, double purchasePrice) {
+        super(name, sellingPrice, purchasePrice,0);
+
+    }
+    public double calculateExpense(){
+        return getPurchasePrice();
+    }
+
+}
