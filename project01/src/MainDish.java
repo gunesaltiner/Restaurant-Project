d4bf79b1c5efa0ce58007eab273779c85846d7f2
Güneş Altıner <gunes.altiner@ozu.edu.tr>
2021-05-17 20:01:57 +0300

    16-MainDish class

---
+++ project01/src/MainDish.java
@@ -0,0 +1,12 @@
+public class MainDish extends Product{
+
+    public MainDish(String name, double sellingPrice, double purchasePrice, double utilityCost){
+        super(name, sellingPrice, purchasePrice, utilityCost);
+    }
+
+    public double calculateExpense(){
+        return getPurchasePrice()+getUtilityCost();
+
+    }
+
+}
