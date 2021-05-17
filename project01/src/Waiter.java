bc65b62902535d9257a4248010bbd9849abd740c
Güneş Altıner <gunes.altiner@ozu.edu.tr>
2021-05-17 19:56:47 +0300

    10-Waiter class/part2

--- project01/src/Waiter.java
+++ project01/src/Waiter.java
@@ -10,6 +10,21 @@ public class Waiter extends Employee {
         super(id, name);
         this.orderRate=0.1;
     }
+    public double calculateExpense() {
+        double exp = 0;
 
+        for (int i = 0; i <= order.size() - 1; i++){
+            exp = exp + order.get(i).calculateTotalPrice();
+        }
+        return exp*orderRate;
+    }
+
+    public void createOrder(Order order){
+        this.order.add(order);
+
+    }
+    public ArrayList<Order> getOrdersReceived(){
+        return order;
+    }
 
 }
