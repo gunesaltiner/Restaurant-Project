0388f7d6a9c9635c4a25a6acd42c170562d3d062
Güneş Altıner <gunes.altiner@ozu.edu.tr>
2021-05-17 20:02:56 +0300

    17-Dessert class

---
+++ project01/src/Dessert.java
@@ -0,0 +1,10 @@
+public class Dessert extends Product{
+
+    public Dessert(String name, double sellingPrice, double purchasePrice, double utilityCost){
+        super(name, sellingPrice, purchasePrice, utilityCost);
+    }
+    public double calculateExpense(){
+        return getPurchasePrice()+getUtilityCost();
+
+    }
+}
